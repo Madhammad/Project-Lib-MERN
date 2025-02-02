fa5d0ccf0d011a4a60b98e88c3c7293bf2961b74
Hammad Rasheed <hammad.naqiali@outlook.com>
2025-02-02 23:03:21 +0500

    utl added

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ mongoose
     console.log(err);
   });
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://project-lib-mern.onrender.com", credentials: true }));
 
 
 
--- client/constant.js
+++ client/constant.js
@@ -1,1 +1,1 @@
-export const URL_BACKEND = "http://localhost:3000"
+export const URL_BACKEND = "https://project-lib-mern.onrender.com"
